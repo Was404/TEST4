456bd9f7f11fab698e9e1a71047120eb3fd46729
Was404 <vladimirovichdanil@bk.ru>
2022-03-09 21:22:55 +0300

    sliders2

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const sliderMain = new Swiper('.slider_main',{
+    freemode: true,
+    centeredSlides:true,
+    mousewheel: true,
+    parallax: true,
+    breakpoints: {
+      0:{
+        slidesPerView:2.5,
+        spaceBetween:20
+    },
+    680: {
+      slidesPerView:3.5,
+      spaceBetween: 60  
+     }
+    }    
+
+
+})
